ed3210d0e8fa4aeb514b3de58866e2dd3ecf05fb
AndriyAO <42350239+AndriyAO@users.noreply.github.com>
2018-11-22 13:08:24 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let {mongoose} = require('./db/mongoose');
+let {User} = require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/users', (req, res) => {
+  var user = new User({
+    name: req.body.name
+  });
+
+user.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/users', (req, res) => {
+  User.find().then((users) => {
+    res.send({users});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.listen(3000, () => {
+    console.log('Started...');
+})
+
+module.exports = {app};
